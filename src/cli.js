01f388b8c83f0d210946be409ab9b8c8f72b1567
David Hermoso <juandavidhermoso@gmail.com>
2020-03-15 17:27:04 +0100

    Build standard message, execute commit, and prevent to commit in protected branches

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,28 @@
 const branchName = require('current-git-branch');
+const git = require('simple-git')();
+const PROTECTED_BRANCHES = ["MASTER", "master", "REL", "rel", "DEV", "dev", "DEVELOPMENT", "development"];
 
-export async function cli(args) {
+export function cli(args) {
     const commitMessage = args[2];
+    const currentBranchName = branchName();
 
-    console.log(branchName())
+    if (!commitMessage) {
+        console.error(`⛔It's difficult to know what you changed. Please, add a commit message.⛔`);
+        return;
+    }
 
+    if (PROTECTED_BRANCHES.includes(currentBranchName)) {
+        console.error(`⛔You are in the wrong branch. You can't directly commit in master, REL or DEV!🔒
+Please, create a feature branch and use a PULL Request.⛔`);
+        return;
+    }
 
+    git.commit(standardCommitMessage(currentBranchName, commitMessage));
+
+    console.log(`Your commit with message "${commitMessage}" was added to branch "${currentBranchName}"`);
+}
+
+function standardCommitMessage(branchName, commitMessage) {
+    return `#${branchName}
+${commitMessage}`;
 }
